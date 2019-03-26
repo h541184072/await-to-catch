1322bbef5c6266048871feb34ca444722e1563bb
h541184072 <541184072@qq.com>
2019-03-26 15:18:43 +0800

    [add] code

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+interface messageUi {
+    error: Function
+}
+
+interface defaultOptions {
+    readonly messageUi: messageUi, // ant Message 组件
+    readonly catchServiceError: Function, // 捕获错误的接口函数
+}
+
+interface urlOptions {
+    apiUrl: string, // 请求的接口地址
+}
+
+export default class AwaitToJs {
+    private defaultOptions: defaultOptions;
+
+    constructor(options: defaultOptions) {
+        this.defaultOptions = options;
+        this.to = this.to.bind(this);
+    }
+
+    to<T>(
+        promise: Promise<T>,
+        options: urlOptions,
+    ): Promise<T | null> {
+        return promise
+            .catch(err => {
+                const {apiUrl} = options;
+
+                const {
+                    catchServiceError,
+                    messageUi,
+                } = this.defaultOptions;
+
+                const params = {
+                    apiUrl,
+                    err: JSON.stringify(err),
+                    url: location.href,
+                };
+                err && messageUi.error(err.message || err);
+                catchServiceError(params);
+
+                return null;
+            });
+    }
+}
